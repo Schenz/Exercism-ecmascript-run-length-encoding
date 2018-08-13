234907fd0dc77ed6e559dd2994891515555c57b9
Schenz <bschenz@gmail.com>
2018-08-13 09:47:35 -0400

    final solution (needs work)

--- run-length-encoding.js
+++ run-length-encoding.js
@@ -1,3 +1,70 @@
-export default function encode(str) {
+export function encode(str) {
+    let strArray, count, previousLetter, output;
 
+    if (str === '') {
+        return '';
+    } else {
+        strArray = str.split('');
+        count = 1;
+        previousLetter = '';
+        output = '';
+        strArray.forEach(letter => {
+            if (letter === previousLetter) {
+                count++;
+            } else {
+                if (count === 1) {
+                    output += previousLetter;
+                } else {
+                    output += `${count}${previousLetter}`;
+                }
+
+                count = 1;
+                previousLetter = letter;
+            }
+        });
+
+        // get the last one
+        if (strArray[strArray.length - 1] === previousLetter) {
+            if (count === 1) {
+                output += previousLetter;
+            } else {
+                output += `${count++}${previousLetter}`;
+            }
+        } else {
+            output += `${strArray[strArray.length]}`;
+        }
+
+        return output;
+    }
+}
+
+export function decode(str) {
+    let strArray, count, output;
+
+    if (str === '') {
+        return ''
+    } else {
+        strArray = str.split('');
+        output = '';
+        count = '';
+        strArray.forEach(letter => {
+            if (isNumeric(letter)) {
+                count += letter;
+            } else {
+                if (count === '') {
+                    output += letter;
+                } else {
+                    output += letter.repeat(count);
+                }
+
+                count = '';
+            }
+        });
+
+        return output;
+    }
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
